73648e8cc4eb1026d3fe94055fd442727671e81d
Arish Raj <arishraj532@gmail.com>
2026-09-17 21:47:10 +0530

    Fix JobCard casing in Home and Jobs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import JobCard from "../components/Jobcard.jsx";
+import JobCard from "../components/JobCard.jsx";
 
 function Home({ onViewJob }) {
   const [searchText, setSearchText] = useState("");
